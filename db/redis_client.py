51c95b35c325c99ffc953048ee555ee2c6fc60f9
Rajesh E <rraj241587@gmail.com>
2025-05-31 11:29:42 +0530

    chnages made

--- db/redis_client.py
+++ db/redis_client.py
@@ -1,4 +1,9 @@
+import os
 import redis
 
 def get_redis_connection():
-    return redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+    redis_url = os.getenv("REDIS_URL")  # Get Redis URL from env variable
+    if not redis_url:
+        # Optionally, you can set a default local Redis URL here for dev
+        redis_url = "redis://localhost:6379"
+    return redis.from_url(redis_url)
